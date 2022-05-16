fa53596fe5bd51bfa89fa1ec31b8731273d50c15
Masahiro noma <macharo.0316@gmail.com>
2022-05-17 00:01:56 +0900

    lesson3追加

---
+++ code_sample/lesson3/ans_kuku.py
@@ -0,0 +1,9 @@
+kuku = []
+for i in range(1, 10):
+    dan = []
+    for j in range(1, 10):
+        dan.append(i*j)
+    kuku.append(dan)
+
+for dan in kuku:
+    print(dan)
---
+++ code_sample/lesson3/hint_kuku.py
@@ -0,0 +1,10 @@
+kuku = []
+for i in range(1, 10):
+
+    for j in range(1, 10):
+        i
+        j
+
+# 九九の表示
+for dan in kuku:
+    print(dan)
